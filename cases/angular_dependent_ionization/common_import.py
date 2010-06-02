7a740c445fb8f899396ebaeb561832c8be66b43d
Raymond Nepstad <raymond.nepstad@gmail.com>
2010-06-02 15:37:50 +0200

    Added case for angular dependent ionization

---
+++ cases/angular_dependent_ionization/common_import.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("../..")
+import helium
+
+sys.path.append(helium.PypropLocation)
+import pyprop
+
+import helium.core.preconditioner
+import helium.core.laserfunctions
+import helium.eigenvalues.eigenstates
+
+#Update Pyprop project namespace with objects from Helium project namespace
+from helium.utils import UpdatePypropProjectNamespace
+UpdatePypropProjectNamespace(pyprop.ProjectNamespace)
+
+__all__ = []

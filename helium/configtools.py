b0a1ddbb974aec3f74043d3d462070f94a47f849
Raymond Nepstad <raymond.nepstad@gmail.com>
2010-06-18 12:45:59 +0200

    Added update function for Config objects

--- helium/configtools.py
+++ helium/configtools.py
@@ -54,4 +54,32 @@ def GetL(conf):
 	return L
 
 
+def UpdateConfig(conf, updateParams):
+	"""
+	Update Pyprop config object with values given in updateParams.
 
+	Input
+	-----
+	conf: pyprop config object
+	updateParams: list of tuples, [('section', 'param', 'value'), ...]
+	
+	
+	Returns
+	-------
+	Updated pyprop config object
+	
+	
+	Note: Section references (i.e. through 'base') will be updated as well.
+
+	"""
+	tmpConf = pyprop.Config(conf)
+	
+	#Update config
+	for section, param, val in updateParams:
+		logging.info("Updating config: %s(%s): %s" % (section, param, val))
+		tmpConf.SetValue(section, param, val)
+
+	#Update config object from possible changed ConfigParser object
+	newConf = pyprop.Config(tmpConf.cfgObj)
+	
+	return newConf
